b22464f8ac9587bb73574673d7e2e2c8cf89ecd0
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-03-03 01:34:36 -0600

    parse simple instructions, gets stuck on [ and ]

--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+
+int stack[100];
+
+int *sp;
+
+#define push(sp, n) (*((sp)++) = (n))
+#define pop(sp) (*--(sp))
 
 void main(void) {
         
-        char hello_world = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+        sp = stack;
+        
+        char hello_world[256] = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
 
         unsigned char cells[30000];
 
         int ptr = 0;
-
-        for(int i = 0; i < strlen(hello_world); ++i) {
+        for(size_t i = 0; i < strlen(hello_world); ++i) {
                 switch(hello_world[i]) {
                     case '>':
                         ++ptr;
@@ -17,21 +26,29 @@ void main(void) {
                         --ptr;
                         break;
                     case '+':
-                        ++cells[ptr] ;
+                        cells[ptr]++;
                         break;
                     case '-':
-                        --cells[ptr] ;
+                        cells[ptr]--;
                         break;
                     case '[':
+                        if (cells[ptr] != 0) {
+                               push(sp, i);
+                        }
                         break;
                     case ']':
+                        if (cells[ptr] != 0) {
+                            size_t x = pop(sp);
+                            i = x;
+                        }
                         break;
-                    case ',':
-                        unsigned char value = getchar();
+                    case ',': ;
+                        char value = getchar();
                         ptr = value;
                         break;
                     case '.':
                         putchar(cells[ptr]);
+                        break;
                     default:
                         continue;
                 }
